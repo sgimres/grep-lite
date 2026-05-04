5a2839e4dc21d3e640f1e1564d387071848e73f6
deepattic <148025215+deepattic@users.noreply.github.com>
2026-05-04 21:27:26 +0530

    parse argument std::env

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let term = std::env::args().nth(1).expect("no term is given");
+    let user_string = std::env::args().nth(2).expect("no string is gizen");
+
+    if user_string.contains(&term) {
+        println!("Pattren {} is found on given String", term);
+    } else {
+        println!("Pattern Not found");
+    }
 }
